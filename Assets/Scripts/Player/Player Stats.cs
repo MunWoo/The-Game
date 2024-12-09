397d0598017022d99c4c4a6b081a24ea3e2dc17f
MunWoo <renatodoessocial@gmail.com>
2024-12-09 02:36:06 +0000

    Test Commit

---
+++ Assets/Scripts/Player/Player Stats.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    public int health;
+    public int maxHealth;
+    public int experience;
+    public int maxExperience;
+}
